9dd2c066d15eb9c48a9551974550bf236377d9b6
srikanthpragada <srikanthpragada@gmail.com>
2020-07-15 19:58:00 +0530

    15-JULY

---
+++ demo/assignments/perfect_number.py
@@ -0,0 +1,14 @@
+# Take a number and display whether it is perfect
+
+num = int(input("Enter a number :"))
+
+total = 1
+for i in range(2, num // 2 + 1):
+    if num % i == 0:
+        total += i
+
+
+if total == num:
+    print("Perfect Number")
+else:
+    print("Not perfect number")
